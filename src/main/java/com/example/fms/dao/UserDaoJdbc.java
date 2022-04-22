b7bd4388a1ef0526b961bbacdc73e9f73f817c6f
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-04-22 17:42:45 +0200

    implementing UserDaoJdbc.java

---
+++ src/main/java/com/example/fms/dao/UserDaoJdbc.java
@@ -0,0 +1,45 @@
+package com.example.fms.dao;
+
+import com.example.fms.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDaoJdbc implements UserDao{
+    @Override
+    public User findUserByemail(String email) {
+
+        User user  = new User();
+        String sql = "select * from users where email =\""+email+"\";";
+        Connection conn = null;
+
+        try{
+            conn = Jdbc.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()){
+                user.setName(rs.getString("name"));
+                user.setBirthdate(rs.getDate("birthdate"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setAddress(rs.getString("address"));
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        finally{
+            Jdbc.closeConnection(conn);
+        }
+        return user;
+    }
+
+    public static void main(String[] args){
+        // quick test
+        UserDao obj = new UserDaoJdbc();
+        User user = obj.findUserByemail("ahmed@gmail.com");
+        System.out.println(user.getName());
+    }
+}
