2d6617effcfa5906a53c77c2f43f4ee17f9e921d
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-04-22 15:50:12 +0200

    changing HelloApp to App

--- src/main/java/com/example/fms/AppController.java
+++ src/main/java/com/example/fms/AppController.java
@@ -3,7 +3,7 @@ package com.example.fms;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.fms {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.fms to javafx.fxml;
