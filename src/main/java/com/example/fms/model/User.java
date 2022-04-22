5fa702d6a1b3fc024269c387473b04855a521598
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-04-22 17:41:18 +0200

    implementing User.java

--- src/main/java/com/example/fms/model/User.java
+++ src/main/java/com/example/fms/model/User.java
@@ -1,4 +1,51 @@
 package com.example.fms.model;
+import java.sql.Date;
 
 public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private Date birthdate;
+
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
