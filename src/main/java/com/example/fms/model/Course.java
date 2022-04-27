b69b60836eb481b298d01ccfae3afca336a6e805
Mohamed Ashraf <mohamed.ashraf.z879@gmail.com>
2022-04-27 15:03:13 +0200

    adding model classes

--- src/main/java/com/example/fms/model/Course.java
+++ src/main/java/com/example/fms/model/Course.java
@@ -1,4 +1,60 @@
 package com.example.fms.model;
 
+import java.util.ArrayList;
+
 public class Course {
+    private int course_ID;
+    private int course_code;
+    private String name;
+    private int credit_hours;
+    private String course_description;
+    private ArrayList<Course> prerequisites;
+
+    public int getCourse_ID() {
+        return course_ID;
+    }
+
+    public int getCourse_code() {
+        return course_code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredit_hours() {
+        return credit_hours;
+    }
+
+    public ArrayList<Course> getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setCourse_ID(int course_ID) {
+        this.course_ID = course_ID;
+    }
+
+    public void setCourse_code(int course_code) {
+        this.course_code = course_code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredit_hours(int credit_hours) {
+        this.credit_hours = credit_hours;
+    }
+
+    public void setPrerequisites(ArrayList<Course> prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public String getCourse_description() {
+        return course_description;
+    }
+
+    public void setCourse_description(String course_description) {
+        this.course_description = course_description;
+    }
 }
--- src/main/java/com/example/fms/model/User.java
+++ src/main/java/com/example/fms/model/User.java
@@ -2,35 +2,27 @@ package com.example.fms.model;
 import java.sql.Date;
 
 public class User {
+    private String id;
     private String name;
     private String email;
     private String password;
     private String address;
     private Date birthdate;
 
-
-    public Date getBirthdate() {
-        return birthdate;
+    public String getId() {
+        return id;
     }
 
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getAddress() {
-        return address;
+    public String getName() {
+        return name;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -41,11 +33,27 @@ public class User {
         this.email = email;
     }
 
-    public String getName() {
-        return name;
+    public String getPassword() {
+        return password;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
     }
 }
