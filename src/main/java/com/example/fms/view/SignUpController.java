233fdfef9f14a8fe7bffe6a55064db11009e38ed
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-04-28 01:50:57 +0200

    adding new views and their controller classes

---
+++ src/main/java/com/example/fms/view/SignUpController.java
@@ -0,0 +1,14 @@
+package com.example.fms.view;
+
+import com.example.fms.App;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class SignUpController {
+    @FXML
+    private Button b;
+}
---
+++ src/main/java/com/example/fms/view/StudentController.java
@@ -0,0 +1,4 @@
+package com.example.fms.view;
+
+public class StudentController {
+}
