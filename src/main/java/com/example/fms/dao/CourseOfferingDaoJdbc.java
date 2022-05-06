e01d22ab9b3d08f0ed26c0040a3fd487d99fa559
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-05-06 10:50:09 +0200

    implementing some functions

--- src/main/java/com/example/fms/dao/CourseOfferingDaoJdbc.java
+++ src/main/java/com/example/fms/dao/CourseOfferingDaoJdbc.java
@@ -16,8 +16,7 @@ public class CourseOfferingDaoJdbc implements CourseOfferingDao {
         ArrayList<CourseOffering> courseOfferings = new ArrayList<>();
 
 
-        String sql= "select * from course_offerings" +
-                "join courses using (course_id)";
+        String sql= "select * from course_offerings join courses using (course_id);";
 
         Connection conn = null;
 
@@ -82,18 +81,100 @@ public class CourseOfferingDaoJdbc implements CourseOfferingDao {
 
     @Override
     public ArrayList<CourseOffering> findApprovedStudentOfferings(Student student) {
-        // TODO Auto-generated method stub
-        return null;
+        String student_ID=student.getId();
+        ArrayList<CourseOffering> courseOfferings = new ArrayList<>();
+        String sql= "select * from course_offerings " +
+                "join takes using(course_offering_id) " +
+                "join courses using(course_id) " +
+                "where id = \""+student_ID+"\" and approved = 1;";
+        Connection conn = null;
+
+        try {
+            conn = Jdbc.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                CourseOffering tempCourse= new CourseOffering();
+                tempCourse.setYear(rs.getInt("year"));
+                tempCourse.setCourse_offering_id(rs.getInt("course_offering_id"));
+                tempCourse.setSemester(CourseOffering.semester_type.valueOf(rs.getString("semester")));
+                tempCourse.setCourse_ID(rs.getInt("course_id"));
+                tempCourse.setCourse_code(rs.getString("course_code"));
+                tempCourse.setName(rs.getString("name"));
+                tempCourse.setCredit_hours(rs.getInt("credit_hours"));
+                tempCourse.setCourse_description(rs.getString("course_description"));
+                courseOfferings.add(tempCourse);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            Jdbc.closeConnection(conn);
+        }
+        return courseOfferings;
     }
 
     @Override
     public ArrayList<CourseOffering> findPendingStudentOfferings(Student student) {
-        // TODO Auto-generated method stub
-        return null;
+        String student_ID=student.getId();
+        ArrayList<CourseOffering> courseOfferings = new ArrayList<>();
+        String sql= "select * from course_offerings " +
+                "join takes using(course_offering_id) " +
+                "join courses using(course_id) " +
+                "where id = \""+student_ID+"\" and approved is null;";
+        Connection conn = null;
+
+        try {
+            conn = Jdbc.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                CourseOffering tempCourse= new CourseOffering();
+                tempCourse.setYear(rs.getInt("year"));
+                tempCourse.setCourse_offering_id(rs.getInt("course_offering_id"));
+                tempCourse.setSemester(CourseOffering.semester_type.valueOf(rs.getString("semester")));
+                tempCourse.setCourse_ID(rs.getInt("course_id"));
+                tempCourse.setCourse_code(rs.getString("course_code"));
+                tempCourse.setName(rs.getString("name"));
+                tempCourse.setCredit_hours(rs.getInt("credit_hours"));
+                tempCourse.setCourse_description(rs.getString("course_description"));
+                courseOfferings.add(tempCourse);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            Jdbc.closeConnection(conn);
+        }
+        return courseOfferings;
     }
     @Override
     public CourseOffering findCourseOffering(String courseCode, String year, String semester) {
-        // TODO Auto-generated method stub
+        String sql= "select * from course_offerings " +
+                "join courses using (course_id) where course_code = \""+courseCode+"\""
+                + "and year = \""+ year + "\" and semester = \"" + semester +"\";";
+
+        Connection conn = null;
+
+        try {
+            conn = Jdbc.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                CourseOffering tempCourse= new CourseOffering();
+                tempCourse.setYear(rs.getInt("year"));
+                tempCourse.setCourse_offering_id(rs.getInt("course_offering_id"));
+                tempCourse.setSemester(CourseOffering.semester_type.valueOf(rs.getString("semester")));
+                tempCourse.setCourse_ID(rs.getInt("course_id"));
+                tempCourse.setCourse_code(rs.getString("course_code"));
+                tempCourse.setName(rs.getString("name"));
+                tempCourse.setCredit_hours(rs.getInt("credit_hours"));
+                tempCourse.setCourse_description(rs.getString("course_description"));
+                return tempCourse;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            Jdbc.closeConnection(conn);
+        }
         return null;
     }
 }
